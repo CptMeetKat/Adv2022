7892c7a74d7d64e5e8e324d2f9127b726826ae3f
Reece <reece.cares@gmail.com>
2022-12-05 17:48:56 +1100

    Completed Day 5

---
+++ D05/D05P1.cs
@@ -0,0 +1,109 @@
+
+public class D05P1 : AocMachine 
+{
+
+   List<Stack<char>> stacks;
+
+   public D05P1(string filename) : base(filename)
+   {
+      readData(filename);
+      stacks = new List<Stack<char>>();
+      int totStacks = (data[0].Length + 1)/4;
+      for(int i = 0; i < totStacks; i++)
+      {
+         stacks.Add(new Stack<char>());
+      }
+      
+   }   
+
+   public override void run()
+   {
+      parseStack();
+      // printStack(); //Empties the stack
+      processStacks();
+      displayResults();
+   }
+
+   private void processStacks()
+   {
+      int start = 0;
+      while(data[start] != ""){start++;}
+      start++;
+
+
+      for(int i = start; i < data.Count; i++)
+      {
+         string[] atoms = data[i].Split(' ');
+
+         int itemsToMove = int.Parse(atoms[1]);
+         int origin = int.Parse(atoms[3]) - 1;
+         int destination = int.Parse(atoms[5]) - 1;
+
+         for(int j = 0; j < itemsToMove; j++)
+         {
+            if(stacks[origin].Count > 0)
+            {
+               char tip = stacks[origin].Pop();
+               stacks[destination].Push   (  tip  );
+            }
+         }
+
+      }
+
+      
+   }
+
+   private void printStack()
+   {
+      foreach (Stack<char> s in stacks)
+      {
+         while(s.Count > 0)
+         {
+            Console.Write(s.Pop());
+         }
+         Console.WriteLine();
+      }
+   }
+
+   private void parseStack()
+   {
+      for(int j = 1; j < data[0].Length; j = j + 4)
+      {
+         //Parse Downwards
+         int down = 0;
+         string crateLetters = "";
+         while(  !Char.IsNumber( data[down][j]  )    )
+         {
+            if(data[down][j] != ' ')
+               crateLetters += data[down][j];
+            down++;
+         }
+         int stackNumber = int.Parse(  data[down][j].ToString()  );
+
+         
+         foreach(char c in crateLetters.Reverse())
+         {
+            stacks[stackNumber-1].Push(c);
+         }
+         // Console.WriteLine("crateLetters {0}", crateLetters);
+      }
+
+   }
+
+
+
+   public override void displayResults()
+   {
+      string result = "";
+      foreach(var s in stacks)
+      {
+         if(s.Count > 0)
+         {
+            result += s.Pop();
+         }
+         
+      }
+      Console.WriteLine("results: {0}", result);
+   }
+}
+
