eeda5870ccdaed4f32489bdd860d443a4af63366
Reece <reece.cares@gmail.com>
2022-12-05 13:46:52 +1100

    Finished Day 4; Corrected file names

---
+++ D04/D04P1.cs
@@ -0,0 +1,45 @@
+
+public class D04P1 : AocMachine 
+{
+   public D04P1(string filename) : base(filename){}   
+
+   public override void run()
+   {
+      readData(filename);     
+      findTotalContainedPairs();
+      displayResults();
+   }
+
+
+   private void findTotalContainedPairs()
+   {
+      int totalContainerPairs = 0;
+
+
+      foreach (string pairs in data)
+      {
+         string[] atoms = pairs.Split(',' , '-');
+         (int, int) range1 = (int.Parse(atoms[0]), int.Parse(atoms[1]));
+         (int, int) range2 = (int.Parse(atoms[2]), int.Parse(atoms[3]));
+
+         if( (   inRange(range1.Item1, range1.Item2, range2.Item1) && inRange(range1.Item1, range1.Item2, range2.Item2) )  ||
+            (inRange(range2.Item1, range2.Item2, range1.Item1) && inRange(range2.Item1, range2.Item2, range1.Item2)))
+         {
+            totalContainerPairs++;
+         }         
+      }
+
+      result = totalContainerPairs;
+   }
+
+   private bool inRange(int first, int last, int target)
+   {
+      return target >= first && target <= last;
+   }
+
+   public override void displayResults()
+   {
+      Console.WriteLine("results: {0}", result);
+   }
+}
+
---
+++ D04/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+List<AocMachine> machines = new List<AocMachine>();
+
+// machines.Add(new D04P1("input2"));
+machines.Add(new D04P1("input"));
+
+
+// machines.Add(new D04P2("input2"));
+machines.Add(new D04P2("input"));
+
+
+foreach (var m in machines)
+   m.run();  
